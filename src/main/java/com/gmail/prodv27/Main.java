630891a0e9c9a60107f12d966b0cb0c524340ce5
Прокопьев Дмитрий Викторович (4080425) <prokopevd@dev.vtb.ru>
2023-02-26 00:42:58 +0400

    added getMessage()

--- src/main/java/com/gmail/prodv27/Main.java
+++ src/main/java/com/gmail/prodv27/Main.java
@@ -2,6 +2,10 @@ package com.gmail.prodv27;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello qa.guru!!!");
+        System.out.println(getMessage());
+    }
+
+    private static String getMessage() {
+        return "Hello qa.guru!!!";
     }
 }
