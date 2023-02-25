476abbfe9d9032f04dca493b0b99a9d83d8a5b02
prodv27 <77414270+prodv27@users.noreply.github.com>
2023-02-26 00:35:57 +0400

    Update Main.java

--- src/main/java/com/gmail/prodv27/Main.java
+++ src/main/java/com/gmail/prodv27/Main.java
@@ -2,6 +2,6 @@ package com.gmail.prodv27;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello qa.guru");
+        System.out.println("Hello qa.guru!!!");
     }
 }
